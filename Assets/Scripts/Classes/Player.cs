0c63afb069986a2d72161270972dff701576bf05
TheWarriorPoet <andrew.jones@students.aie.edu.au>
2015-05-14 20:42:27 +1000

    Player runs and Jumps

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -3,19 +3,29 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    public float Speed = 10.0f;
+    public float JumpForce = 100.0f;
 
-
+    private GameManager _myGameManager = null;
+    private Rigidbody2D _myRigidBody2D = null;
+    private Transform _myTransform = null;
+    private Vector2 position = Vector2.zero;
 
 	// Use this for initialization
 	void Start () {
-	
+        _myGameManager = GameManager.instance;
+        _myRigidBody2D = GetComponent<Rigidbody2D>();
+        _myTransform = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("Jump"))
+        if (Input.GetButtonDown("Jump") && _myRigidBody2D.velocity.y == 0)
         {
-            
+            if (_myRigidBody2D != null) { _myRigidBody2D.AddRelativeForce(new Vector2(0.0f, JumpForce)); } else Debug.Log("_myRigidBody2D is null");
         }
+        float translation = Input.GetAxis("Horizontal") * Speed;
+        translation *= Time.deltaTime;
+        _myTransform.Translate(translation, 0, 0);
 	}
 }
--- Assets/Scripts/Classes/SceneManager_Base.cs
+++ Assets/Scripts/Classes/SceneManager_Base.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class SceneManager_Base : MonoBehaviour {
 
+
+    protected GameManager _myGameManager;
+
 	// Use this for initialization
 	void Start () {
-	
+        _myGameManager = GameManager.instance;
 	}
 	
 	// Update is called once per frame
