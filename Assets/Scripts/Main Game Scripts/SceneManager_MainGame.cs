b15be723ea17aebda57421d879c17bf6e2b851e6
TheWarriorPoet <andrew.jones@students.aie.edu.au>
2015-05-14 18:12:56 +1000

    Initial Class Files

---
+++ Assets/Scripts/Main Game Scripts/SceneManager_MainGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager_MainGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
