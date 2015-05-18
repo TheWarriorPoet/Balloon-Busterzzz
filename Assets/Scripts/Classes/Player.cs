cf306676de4839c281da5243bfef7ba1bc716e0f
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-05-18 21:36:41 +1000

    Added Force to the fireball based on facing direction. Causing the ball to travel forward.

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour {
     public float Speed = 10.0f;
     public float JumpForce = 100.0f;
 
+	public bool IsLeft;
+
     private GameManager _myGameManager = null;
     private Rigidbody2D _myRigidBody2D = null;
     private Transform _myTransform = null;
@@ -22,13 +24,29 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetAxis ("Horizontal") < 0) {
+			IsLeft = true;
+		}
+		if (Input.GetAxis ("Horizontal") > 0) {
+			IsLeft = false;
+		}
         if (Input.GetButtonDown("Jump") && _myRigidBody2D.velocity.y == 0)
         {
             if (_myRigidBody2D != null) { _myRigidBody2D.AddRelativeForce(new Vector2(0.0f, JumpForce)); } else Debug.Log("_myRigidBody2D is null");
         }
 		if(Input.GetButtonDown("Fire1"))
 		{
-			GameObject tempObj = (GameObject)Instantiate(BalloonScript.Power.Primary,gameObject.transform.position , gameObject.transform.rotation);
+
+			if(IsLeft)
+			{
+				Vector2 tempPos = gameObject.transform.position;
+				tempPos.x -= 1;
+			GameObject tempObj = (GameObject)Instantiate(BalloonScript.Power.Primary, tempPos, gameObject.transform.rotation);
+			tempObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000,0));
+			}else{
+			GameObject tempObj = (GameObject)Instantiate(BalloonScript.Power.Primary, gameObject.transform.position , gameObject.transform.rotation);
+			tempObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000,0));
+			}
 		}
         float translation = Input.GetAxis("Horizontal") * Speed;
         translation *= Time.deltaTime;
