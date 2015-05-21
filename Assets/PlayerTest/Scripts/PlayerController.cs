2340aa3f291b2e4f86c0772e25a2dfb0afb38243
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-05-21 19:04:19 +1000

    Added firing to the PlayerController, Adjusted the key input to use Unity's Input Binding system,

--- Assets/PlayerTest/Scripts/PlayerController.cs
+++ Assets/PlayerTest/Scripts/PlayerController.cs
@@ -48,21 +48,54 @@ public class PlayerController : MonoBehaviour {
 
 		switch (playerID) {
 		case 0:
-			if (Input.GetKey (KeyCode.D)) {
-				MoveLeft();
+			if (Input.GetAxis ("HorizontalP1") > 0) {
+				MoveLeft ();
 			}
-
-			if (Input.GetKey (KeyCode.A)) {
-				MoveRight();
+			if (Input.GetAxis ("HorizontalP1") < 0) {
+				MoveRight ();
 			}
+			if(Input.GetButtonDown("Fire1P1"))
+			{
+				if(isLeft)
+				{
+					Vector2 tempPos = gameObject.transform.position;
+					tempPos.x -= 1;
+					GameObject tempObj = (GameObject)Instantiate(GetComponent<Balloon>().Power.Primary, tempPos, gameObject.transform.rotation);
+					Vector2 tempVec = characterController.velocity;
+					tempVec.x += -1000;
+					tempObj.GetComponent<Rigidbody2D>().AddForce(tempVec);
+				}else{
+					GameObject tempObj = (GameObject)Instantiate(GetComponent<Balloon>().Power.Primary, gameObject.transform.position , gameObject.transform.rotation);
+					Vector2 tempVec = characterController.velocity;
+					tempVec.x += 1000;
+					tempObj.GetComponent<Rigidbody2D>().AddForce(tempVec);
+				}
+			}
+		
 			break;
 		case 1:
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				MoveLeft();
+			if (Input.GetAxis ("HorizontalP2") > 0) {
+				MoveLeft ();
 			}
-			
-			if (Input.GetKey (KeyCode.LeftArrow)) {
-				MoveRight();
+			if (Input.GetAxis ("HorizontalP2") < 0) {
+				MoveRight ();
+			}
+			if(Input.GetButtonDown("Fire1P2"))
+			{
+				if(isLeft)
+				{
+					Vector2 tempPos = gameObject.transform.position;
+					tempPos.x -= 1;
+					GameObject tempObj = (GameObject)Instantiate(GetComponent<Balloon>().Power.Primary, tempPos, gameObject.transform.rotation);
+					Vector2 tempVec = characterController.velocity;
+					tempVec.x += -1000;
+					tempObj.GetComponent<Rigidbody2D>().AddForce(tempVec);
+				}else{
+					GameObject tempObj = (GameObject)Instantiate(GetComponent<Balloon>().Power.Primary, gameObject.transform.position , gameObject.transform.rotation);
+					Vector2 tempVec = characterController.velocity;
+					tempVec.x += 1000;
+					tempObj.GetComponent<Rigidbody2D>().AddForce(tempVec);
+				}
 			}
 			break;
 		}
