69b48e4f73ec051ed9b9606cb3a9a6660de9a06c
Zyte <gcheffirs@gmail.com>
2015-05-21 17:06:42 +0930

    Added ability for two players at once

--- Assets/PlayerTest/Scripts/PlayerController.cs
+++ Assets/PlayerTest/Scripts/PlayerController.cs
@@ -12,39 +12,59 @@ public class PlayerController : MonoBehaviour {
 	private Vector2 velocity = new Vector2(0f,0f);
 	private bool isLeft = true;
 	private bool running = false;
+	public int playerID = 0;
 
 
 	void Start () {
 		characterController = this.GetComponent<CharacterController> ();
 	
 	}
-	
+
+	void MoveLeft()
+	{
+		velocity.x = CharacterSpeed * Time.deltaTime;
+		if (isLeft) {
+			transform.Rotate (new Vector3 (0f, 180f, 0f));
+			isLeft = false;
+			
+		}
+		running = true;
+	}
+
+	void MoveRight()
+	{
+		velocity.x = CharacterSpeed * Time.deltaTime * -1;
+		if (!isLeft) {
+			transform.Rotate (new Vector3 (0f, 180f, 0f));
+			isLeft = true;				       
+		}
+		running = true;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		velocity.x = 0;
 		running = false;
 
-		if (Input.GetKey(KeyCode.D))
-		{
-			velocity.x = CharacterSpeed * Time.deltaTime;
-			if (isLeft)
-			{
-				transform.Rotate(new Vector3(0f,180f,0f));
-				isLeft = false;
-				
+		switch (playerID) {
+		case 0:
+			if (Input.GetKey (KeyCode.D)) {
+				MoveLeft();
 			}
-			running = true;
-		}
 
-		if (Input.GetKey(KeyCode.A))
-		{
-			velocity.x = CharacterSpeed * Time.deltaTime * -1;
-			if (!isLeft)
-			{
-				transform.Rotate(new Vector3(0f,180f,0f));
-				isLeft = true;				       
+			if (Input.GetKey (KeyCode.A)) {
+				MoveRight();
 			}
-			running = true;
+			break;
+		case 1:
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				MoveLeft();
+			}
+			
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				MoveRight();
+			}
+			break;
 		}
 
 		characterController.SimpleMove (velocity);
