b0b9bca380f630b5710d71d382a35f1f739837aa
Zyte <gcheffirs@gmail.com>
2015-05-28 16:58:17 +0930

    Check balloon health for ability to fly

--- Assets/PlayerTest/Scripts/PlayerController.cs
+++ Assets/PlayerTest/Scripts/PlayerController.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
 	public float CharacterSpeed = 470.0f;
 	public Animator CharacterAnimator;
 	public float JumpForce = 10f;
@@ -21,9 +21,9 @@ public class PlayerController : MonoBehaviour {
 	private float FireCooldown;
 
 
-	void Start () {
-		characterController = this.GetComponent<CharacterController> ();
-	
+	void Start ()
+	{
+		characterController = this.GetComponent<CharacterController>();
 	}
 
 	void MoveLeft()
@@ -32,7 +32,6 @@ public class PlayerController : MonoBehaviour {
 		if (isLeft) {
 			transform.Rotate (new Vector3 (0f, 180f, 0f));
 			isLeft = false;
-			
 		}
 		running = true;
 	}
@@ -48,18 +47,32 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		velocity.x = 0;
 		running = false;
 		jumping = (characterController.collisionFlags == CollisionFlags.None);
 		FireCooldown -= Time.deltaTime;
 
-		switch (playerID) {
+		// Check balloon health for ability to fly
+		if (GetComponent<Balloon>().m_fHealth == 0 && CanFly)
+		{
+			CanFly = false;
+		}
+		else if (GetComponent<Balloon>().m_fHealth > 0 && !CanFly)
+		{
+			CanFly = true;
+		}
+
+		switch (playerID)
+		{
 		case 0:
-			if (Input.GetAxis ("HorizontalP1") > 0) {
+			if (Input.GetAxis ("HorizontalP1") > 0)
+			{
 				MoveLeft ();
 			}
-			if (Input.GetAxis ("HorizontalP1") < 0) {
+			if (Input.GetAxis ("HorizontalP1") < 0)
+			{
 				MoveRight ();
 			}
 			if(Input.GetAxis("JumpP1") >0  && !jumping)
@@ -67,19 +80,18 @@ public class PlayerController : MonoBehaviour {
 				velocity.y =+ JumpForce;
 				jumping = true;
 			}
-
 			if(Input.GetButtonDown("Fire1P1"))
 			{
 				FireProj ();
-
 			}
-		
 			break;
 		case 1:
-			if (Input.GetAxis ("HorizontalP2") > 0) {
+			if (Input.GetAxis ("HorizontalP2") > 0)
+			{
 				MoveLeft ();
 			}
-			if (Input.GetAxis ("HorizontalP2") < 0) {
+			if (Input.GetAxis ("HorizontalP2") < 0)
+			{
 				MoveRight ();
 			}
 			if(Input.GetAxis("JumpP2") >0  && !jumping)
@@ -90,7 +102,6 @@ public class PlayerController : MonoBehaviour {
 			if(Input.GetButtonDown("Fire1P2"))
 			{
 				FireProj ();
-
 			}
 			break;
 		}
@@ -103,23 +114,19 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		characterController.Move(new Vector2(velocity.x * Time.deltaTime,velocity.y*Time.deltaTime));
-	
-
-
-
 
 		CharacterAnimator.SetBool("running",running);
 		CharacterAnimator.SetBool("jumping",jumping);
 		//print (velocity.y.ToString ());
 		CharacterAnimator.SetFloat ("yVel",velocity.y);
-
-
 	}
 
 	void FireProj()
 	{
-		if (FireCooldown < 0) {
-			if (isLeft) {
+		if (FireCooldown < 0)
+		{
+			if (isLeft)
+			{
 				Vector2 tempPos = gameObject.transform.position;
 				tempPos.x -= 1;
 				GameObject tempObj = (GameObject)Instantiate (GetComponent<Balloon> ().Power.Primary, tempPos, gameObject.transform.rotation);
@@ -127,7 +134,9 @@ public class PlayerController : MonoBehaviour {
 				Vector2 tempVec = characterController.velocity;
 				tempVec.x += -1000;
 				tempObj.GetComponent<Rigidbody> ().AddForce (tempVec);
-			} else {
+			}
+			else
+			{
 				GameObject tempObj = (GameObject)Instantiate (GetComponent<Balloon> ().Power.Primary, gameObject.transform.position, gameObject.transform.rotation);
 				tempObj.transform.GetChild (0).GetComponent<Projectile> ().m_oCreator = playerID;
 				Vector2 tempVec = characterController.velocity;
@@ -137,7 +146,4 @@ public class PlayerController : MonoBehaviour {
 			FireCooldown = gameObject.GetComponent<Balloon>().Power.Cooldown;
 		}
 	}
-
-
-
 }
